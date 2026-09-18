c3738d976c6023eb2a0372936ee31b2c6507663f
anujshandillya <anujsharmawork11@gmail.com>
2026-09-18 17:12:18 +0530

    cleanup

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config({ path: new URL('../.env', import.meta.url).pathname });
+// import dotenv from 'dotenv';
+// dotenv.config({ path: new URL('../.env', import.meta.url).pathname });
 const MONGO_URI = "mongodb://localhost:27017/notes_db"
 // const MONGO_URI = process.env.MONGO_URI;
 export default async function ConnectToDB() {
